dda3c5368de246025c0c54e15c15d19645036490
afeiship <afeiship@gmail.com>
2019-01-17 14:36:56 +0800

    refactor: use new impl

--- src/next-is-plain-object.js
+++ src/next-is-plain-object.js
@@ -1,28 +1,19 @@
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
-  var isFunction = function(inFn) {
-    return typeof inFn === 'function';
-  };
-  var fnToString = function(inFn) {
-    return Function.prototype.toString.call(inFn);
-  };
 
   nx.isPlainObject = function(inTarget) {
-    var proto, constructor;
-    if (!inTarget || typeof inTarget !== 'object') {
-      return false;
+    var proto, baseProto;
+    if (!inTarget || typeof inTarget !== 'object') return false;
+    proto = Object.getPrototypeOf(inTarget);
+    baseProto = proto;
+
+    if (proto === null) return true;
+    while (proto !== null) {
+      proto = Object.getPrototypeOf(proto);
     }
-    proto = isFunction(inTarget.constructor) ? Object.getPrototypeOf(inTarget) : Object.prototype;
-    if (proto === null) {
-      return true;
-    }
-    constructor = proto.constructor;
-    return (
-      isFunction(constructor) &&
-      constructor instanceof constructor &&
-      fnToString(constructor) === fnToString(Object)
-    );
+
+    return Object.getPrototypeOf(inTarget) === baseProto;
   };
 
   if (typeof module !== 'undefined' && module.exports) {
