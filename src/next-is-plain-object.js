7e413c4bc4ab7c1b3c984f9429fcdc30fc4fe6b2
afeiship <1290657123@qq.com>
2020-11-04 09:28:00 +0800

    fix: use compose solution

--- src/next-is-plain-object.js
+++ src/next-is-plain-object.js
@@ -1,22 +1,40 @@
 (function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
+
   // https://github.com/reduxjs/react-redux/blob/master/src/utils/isPlainObject.js
+  // https://github.com/jonschlinkert/is-plain-object
+
+  function isObject(val) {
+    return val != null && typeof val === 'object' && Array.isArray(val) === false;
+  }
+
+  function isObjectObject(o) {
+    return isObject(o) === true && Object.prototype.toString.call(o) === '[object Object]';
+  }
 
   nx.isPlainObject = function (inTarget) {
-    if (!inTarget || typeof inTarget !== 'object') return false;
+    var ctor, prot;
 
-    var proto = Object.getPrototypeOf(inTarget);
-    var baseProto = proto;
-    if (proto === null) return true;
-    var ctor = inTarget.constructor;
+    if (!inTarget || typeof inTarget !== 'object') return false;
+    if (Object.getPrototypeOf(inTarget) === null) return true;
+    if (isObjectObject(inTarget) === false) return false;
+
+    // If has modified constructor
+    ctor = inTarget.constructor;
     if (typeof ctor !== 'function') return false;
 
-    while (proto !== null) {
-      proto = Object.getPrototypeOf(proto);
+    // If has modified prototype
+    prot = ctor.prototype;
+    if (isObjectObject(prot) === false) return false;
+
+    // If constructor does not have an Object-specific method
+    if (prot.hasOwnProperty('isPrototypeOf') === false) {
+      return false;
     }
 
-    return Object.getPrototypeOf(inTarget) === baseProto;
+    // Most likely a plain Object
+    return true;
   };
 
   if (typeof module !== 'undefined' && module.exports) {
